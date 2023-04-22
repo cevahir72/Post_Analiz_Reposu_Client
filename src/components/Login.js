b1a67d984c160ea0a64bbd823e543d42d08832d2
Mevlut KELES <87367047+cevahir72@users.noreply.github.com>
2023-04-22 11:59:15 +0300

    appContent

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,12 +19,12 @@ const Login = () => {
   };
 
   return (
-    <section className="vh-100">
-    <div className="container-fluid h-custom mt-4">
+    <div className="vh-100 mx-4">
+    <div className="container-fluid h-custom mx-4" >
       <div className="row d-flex justify-content-center align-items-center h-100">
-        <div className="col-md-9 col-lg-6 col-xl-5">
+        <div className=" col-md-9 col-lg-6 col-xl-5">
           <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-            className="img-fluid" alt="Sample image"/>
+            className="img-fluid" alt="Sample"/>
         </div>
         <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
           <form>
@@ -87,7 +87,7 @@ const Login = () => {
       </div>
     </div>
     
-  </section>
+  </div>
   );
 };
 
--- src/views/contents/AppHeader.js
+++ src/views/contents/AppHeader.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const AppHeader = () => {
   return (
-    <div>AppHeader</div>
+    <Navbar
+    collapseOnSelect
+    expand="lg"
+    style={{ background: "#CD9B4F", color: "white" ,marginBottom:"3rem"}}
+    variant="dark"
+  >
+    <Container>
+      <Navbar.Brand  style={{ color: "white" }} href="#home">
+        <b>Furni-Scope</b> 
+      </Navbar.Brand>
+      <Navbar.Toggle  aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse  id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link style={{ color: "white" }} href="/post">
+            Post
+          </Nav.Link>
+          <Nav.Link style={{ color: "white" }} href="/financing">
+            Financing
+          </Nav.Link>
+          <Nav.Link style={{ color: "white" }} href="/login">
+            Product
+          </Nav.Link>
+          <Nav.Link style={{ color: "white" }} href="/analyze">
+            Analyze
+          </Nav.Link>
+          {/* <Nav.Link style={{ color: "white" }} href="/admin">
+            Admin
+          </Nav.Link> */}
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   )
 }
 
