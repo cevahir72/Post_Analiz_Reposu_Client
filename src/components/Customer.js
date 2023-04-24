acf735767429153bb7fd6ea625fa2d1c38753391
Mevlut KELES <87367047+cevahir72@users.noreply.github.com>
2023-04-25 00:57:20 +0300

    page-changes

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState } from "react";
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+
+const mockCustomer = [
+  {
+    id:1,
+    question: "Pazarlık",
+    answer: "I have no authorization to negotiate. But if you share your number, i can direct it to my manager."
+  }
+]
 
 const Customer = () => {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div>
-        <h1>Customer</h1>
+    <div
+      className="container justify-content-center "
+      style={{ minHeight: "87vh" }}
+    >
+      <div>
+        <button
+          type="submit"
+          className="btn btn-warning"
+          onClick={handleShow}
+          style={{ background: "#CD9B4F", color: "white" }}
+        >
+          <i class="fa-solid fa-plus"></i> Add Customer Answer
+        </button>
+      </div>
+      <div className="row mt-4">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col">
+            <div className="card">
+              <div className="card-body p-5">
+                {mockCustomer.map((item) => (
+                  <div
+                    class="row my-2 py-2 d-flex align-items-center "
+                    style={{
+                      border: "1px solid #ddd",
+                      borderRadius: "5px",
+                    }}
+                  >
+                      <strong>{item.question}</strong>
+                      <p>{item.answer}</p>
+                      <div class="col-sm-12 d-flex justify-content-end  col-md-12 col-lg-12">
+                      <button
+                        type="submit"
+                        className="btn btn-warning"
+                        style={{ background: "#CD9B4F", color: "white" }}
+                      >
+                        <i class="fa-solid fa-plus"></i>
+                      </button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {show && (
+        <Offcanvas
+          show={show}
+          onHide={handleClose}
+          placement="end"
+          style={{ width: "750px" }}
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Add Customer Answer</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>Buraya Form Gelecek</Offcanvas.Body>
+        </Offcanvas>
+      )}
     </div>
   )
 }
